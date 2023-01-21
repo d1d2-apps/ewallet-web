71a04cb632648621ddb3b76fa2e56624d105e4ac
Diego Ferreira <diegosf1705@gmail.com>
2023-01-21 16:58:28 -0300

    Improved BillsList component with Select

--- src/features/bills/components/BillsList/BillsList.tsx
+++ src/features/bills/components/BillsList/BillsList.tsx
@@ -1,25 +1,45 @@
+import { useState } from 'react';
+
+import { Select } from '@/components/elements';
 import { EmptyFeedback, LoadingFeedback } from '@/components/feedbacks';
 
 import { useBills } from '../../api/getBills';
 import billsImg from '../../assets/images/bills.png';
+import * as S from './BillsList.styles';
 
 export function BillsList() {
   const billsQuery = useBills();
 
-  if (billsQuery.isLoading || billsQuery.isFetching) {
-    return <LoadingFeedback title="Carregando faturas..." />;
-  }
+  const [selectedCreditCardId, setSelectedCreditCardId] = useState('');
 
-  if (!billsQuery.data?.length) {
-    return (
-      <EmptyFeedback
-        image={billsImg}
-        title="Ooops! Está vazio"
-        description="Parece que você não tem nenhuma fatura lançada ainda."
-      />
-    );
-  }
+  return (
+    <S.Container>
+      <Select.Root value={selectedCreditCardId} onValueChange={value => setSelectedCreditCardId(value)}>
+        <Select.Group>
+          <Select.GroupLabel>Cartão de crédito</Select.GroupLabel>
 
-  // return <BillsTable data={billsQuery.data} />;
-  return <h3>BillsTable</h3>;
+          <Select.Item value="">Todos</Select.Item>
+        </Select.Group>
+      </Select.Root>
+
+      {(() => {
+        if (billsQuery.isLoading || billsQuery.isFetching) {
+          return <LoadingFeedback title="Carregando faturas..." />;
+        }
+
+        if (!billsQuery.data?.length) {
+          return (
+            <EmptyFeedback
+              image={billsImg}
+              title="Ooops! Está vazio"
+              description="Parece que você não tem nenhuma fatura lançada ainda."
+            />
+          );
+        }
+
+        // return <BillsTable data={billsQuery.data} />;
+        return <h3>BillsTable</h3>;
+      })()}
+    </S.Container>
+  );
 }
--- src/features/bills/routes/Bills/BillsPage.tsx
+++ src/features/bills/routes/Bills/BillsPage.tsx
@@ -1,25 +1,30 @@
 import { FiPlus } from 'react-icons/fi';
 
 import { Button, Heading } from '@/components/elements';
+import { Head } from '@/components/head';
 
 import { BillsList } from '../../components/BillsList/BillsList';
 import * as S from './BillsPage.styles';
 
 export function BillsPage() {
   return (
-    <S.Container>
-      <header>
-        <div>
-          <Heading>Faturas</Heading>
-          <Heading variant="h4" asChild>
-            <h4>Gerencie todos as suas faturas ou faça novos lançamentos.</h4>
-          </Heading>
-        </div>
+    <>
+      <Head title="Faturas" />
 
-        <Button leftIcon={FiPlus}>Lançar fatura</Button>
-      </header>
+      <S.Container>
+        <header>
+          <div>
+            <Heading>Faturas</Heading>
+            <Heading variant="h4" asChild>
+              <h4>Gerencie todos as suas faturas ou faça novos lançamentos.</h4>
+            </Heading>
+          </div>
 
-      <BillsList />
-    </S.Container>
+          <Button leftIcon={FiPlus}>Lançar fatura</Button>
+        </header>
+
+        <BillsList />
+      </S.Container>
+    </>
   );
 }
