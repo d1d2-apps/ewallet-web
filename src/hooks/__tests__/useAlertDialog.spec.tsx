01d6b9e270be43c3ba8595d0f406afc8b1b55de6
Diego Ferreira <diegosf1705@gmail.com>
2023-03-17 21:46:06 -0300

    test: refactor useAlertDialog hook test code to not use nesting

--- src/hooks/__tests__/useAlertDialog.spec.tsx
+++ src/hooks/__tests__/useAlertDialog.spec.tsx
@@ -1,24 +1,24 @@
 import { AppProvider } from '@/providers/app';
-import { act, renderHook, screen, userEvent } from '@/test/test-utils';
+import { act, cleanup, renderHook, screen, userEvent } from '@/test/test-utils';
 
 import { useAlertDialog } from '../useAlertDialog';
 
-describe('useAlertDialog Hook', () => {
-  it('should render without crashing', async () => {
-    const { result } = renderHook(() => useAlertDialog(), { wrapper: AppProvider });
+afterEach(() => cleanup());
 
-    act(() => {
-      result.current.show({
-        title: 'Warning!',
-        description: 'This action was executed successfully.',
-        okButtonLabel: 'Close'
-      });
+test('calls show and close the dialog properly', async () => {
+  const { result } = renderHook(() => useAlertDialog(), { wrapper: AppProvider });
+
+  act(() => {
+    result.current.show({
+      title: 'Warning!',
+      description: 'This action was executed successfully.',
+      okButtonLabel: 'Close'
     });
-
-    expect(screen.getByRole('alertdialog')).toBeInTheDocument();
-
-    await userEvent.click(screen.getByText('Close'));
-
-    expect(screen.queryByRole('alertdialog')).not.toBeInTheDocument();
   });
+
+  expect(screen.getByRole('alertdialog')).toBeInTheDocument();
+
+  await userEvent.click(screen.getByText('Close'));
+
+  expect(screen.queryByRole('alertdialog')).not.toBeInTheDocument();
 });
