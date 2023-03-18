d05edd7f8a61e2a9f6bdb56e783904f87166ecff
Diego Ferreira <diegosf1705@gmail.com>
2023-03-17 21:40:18 -0300

    test: refactor alert dialog component test code to not use nesting

--- src/components/dialogs/AlertDialog/AlertDialog.spec.tsx
+++ src/components/dialogs/AlertDialog/AlertDialog.spec.tsx
@@ -1,19 +1,19 @@
 import { ThemeProvider } from 'styled-components';
 
 import { themes } from '@/config/styles/themes';
-import { rtlRender } from '@/test/test-utils';
+import { cleanup, rtlRender } from '@/test/test-utils';
 
 import { AlertDialog } from './AlertDialog';
 
-describe('AlertDialog Component', () => {
-  it('should render without crashing', async () => {
-    const { getByRole } = rtlRender(
-      <AlertDialog open title="Warning!" description="This action was executed successfully." okButtonLabel="Close" />,
-      {
-        wrapper: ({ children }) => <ThemeProvider theme={themes.light}>{children}</ThemeProvider>
-      }
-    );
+afterEach(() => cleanup());
 
-    expect(getByRole('alertdialog')).toBeInTheDocument();
-  });
+test('shows properly', async () => {
+  const { getByRole } = rtlRender(
+    <AlertDialog open title="Warning!" description="This action was executed successfully." okButtonLabel="Close" />,
+    {
+      wrapper: ({ children }) => <ThemeProvider theme={themes.light}>{children}</ThemeProvider>
+    }
+  );
+
+  expect(getByRole('alertdialog')).toBeInTheDocument();
 });
