46b8ac4340b2c2d979e9a9a7582fb85194b247ea
Diego Ferreira <diegosf1705@gmail.com>
2023-06-11 18:45:53 -0300

    refactor(CreateCreditCardModal): implement Dialog component

--- src/features/creditCards/components/CreateCreditCardModal/CreateCreditCardModal.tsx
+++ src/features/creditCards/components/CreateCreditCardModal/CreateCreditCardModal.tsx
@@ -1,19 +1,18 @@
 import { useForm } from 'react-hook-form';
-import { FiUser, FiX } from 'react-icons/fi';
 import { toast } from 'react-toastify';
 
 import { yupResolver } from '@hookform/resolvers/yup';
+import { CreditCard as CreditCardIcon } from '@phosphor-icons/react';
 import * as DialogPrimitive from '@radix-ui/react-dialog';
 import * as yup from 'yup';
 
-import { Button } from '@/components/elements';
+import { Dialog } from '@/components/elements';
 import { ControlledTextInput } from '@/components/forms';
 import { useAlertDialog } from '@/hooks';
 
 import { CreateCreditCardDTO, useCreateCreditCard } from '../../api/createCreditCard';
 import { useUpdateCreditCard } from '../../api/updateCreditCard';
 import { CreditCard } from '../../types';
-import * as S from './CreateCreditCardModal.styles';
 
 type FormData = CreateCreditCardDTO['data'];
 
@@ -70,55 +69,30 @@ export function CreateCreditCardModal({
   };
 
   return (
-    <DialogPrimitive.Root {...rest}>
-      <DialogPrimitive.Portal>
-        <S.Overlay />
+    <Dialog.Root {...rest} asChild>
+      <form onSubmit={handleSubmit(handleSaveCreditCard)}>
+        <Dialog.Header title={`${creditCard?.id ? 'Editar' : 'Cadastrar'} cartão de crédito`} />
 
-        <S.Content asChild>
-          <form onSubmit={handleSubmit(handleSaveCreditCard)}>
-            <header>
-              <S.Title>{creditCard?.id ? 'Editar' : 'Cadastrar'} cartão de crédito</S.Title>
+        <Dialog.Body>
+          <ControlledTextInput
+            name="name"
+            control={control}
+            label="Nome"
+            icon={CreditCardIcon}
+            placeholder="Ex.: Nubank, Inter, Itaú..."
+          />
+        </Dialog.Body>
 
-              <DialogPrimitive.Close asChild>
-                <Button size="xs" colorScheme="neutral" isRounded>
-                  <FiX />
-                </Button>
-              </DialogPrimitive.Close>
-            </header>
-
-            <main>
-              <ControlledTextInput
-                name="name"
-                control={control}
-                label="Nome"
-                icon={FiUser}
-                placeholder="Ex.: Nubank, Inter, Itaú..."
-              />
-            </main>
-
-            <footer>
-              <DialogPrimitive.Close asChild>
-                <Button
-                  colorScheme="white"
-                  size="sm"
-                  disabled={createCreditCardMutation.isLoading || updateCreditCardMutation.isLoading}
-                >
-                  Fechar
-                </Button>
-              </DialogPrimitive.Close>
-
-              <Button
-                type="submit"
-                size="sm"
-                isLoading={createCreditCardMutation.isLoading || updateCreditCardMutation.isLoading}
-                loadingText="Salvando..."
-              >
-                Salvar
-              </Button>
-            </footer>
-          </form>
-        </S.Content>
-      </DialogPrimitive.Portal>
-    </DialogPrimitive.Root>
+        <Dialog.Footer
+          closeButtonOptions={{
+            disabled: createCreditCardMutation.isLoading || updateCreditCardMutation.isLoading
+          }}
+          primaryButtonOptions={{
+            isLoading: createCreditCardMutation.isLoading || updateCreditCardMutation.isLoading,
+            loadingText: 'Salvando...'
+          }}
+        />
+      </form>
+    </Dialog.Root>
   );
 }
