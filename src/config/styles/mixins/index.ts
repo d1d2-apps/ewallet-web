2a320d9c422b4239b9b2354bf7bfc198f98f4fc1
Diego Ferreira <diegosf1705@gmail.com>
2023-06-12 19:58:41 -0300

    chore(mixins): remove unused dialogs mixins

--- src/config/styles/mixins/index.ts
+++ src/config/styles/mixins/index.ts
@@ -1,11 +1,9 @@
-import { dialogs } from './dialogs';
 import { ellipsize } from './ellipsize';
 import { ring } from './rings';
 import { screen } from './screen';
 
 export const mixins = {
   screen,
-  dialogs,
   ring,
   ellipsize
 };
