dd9f74079f0070a2d1c3e350e8a5aedd27d0d975
Diego Ferreira <diegosf1705@gmail.com>
2023-06-14 19:24:38 -0300

    chore(CreateBillPage): create stepper elements

--- src/features/bills/routes/CreateBill/CreateBillPage.styles.ts
+++ src/features/bills/routes/CreateBill/CreateBillPage.styles.ts
@@ -1,3 +1,79 @@
+import { opacify } from 'polished';
 import styled from 'styled-components';
 
-export const Container = styled.main``;
+interface StepProps {
+  $isCurrentStep?: boolean;
+}
+
+export const Container = styled.div`
+  height: 100%;
+  max-width: 1200px;
+  margin: 0 auto;
+
+  display: flex;
+  flex-direction: column;
+  gap: 3rem;
+`;
+
+export const StepperWrapper = styled.main`
+  flex: 1;
+
+  display: flex;
+  gap: 1.5rem;
+`;
+
+export const Stepper = styled.aside`
+  height: 100%;
+
+  position: relative;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 3rem;
+
+  &::after {
+    content: '';
+    width: 1px;
+    background-color: red;
+    background: ${({ theme }) =>
+      `linear-gradient(180deg, transparent 0%, ${opacify(-0.85, theme.colors.neutral)} 50%, transparent 100%)`};
+
+    position: absolute;
+    top: 0;
+    right: 1.25rem;
+    bottom: 0;
+  }
+`;
+
+export const Step = styled.article<StepProps>`
+  width: 100%;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 2.5rem;
+
+  & > main {
+    flex: 1;
+
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    gap: 0.5rem;
+
+    & > strong {
+      color: ${({ theme }) => opacify(-0.25, theme.colors.neutral)};
+      font-weight: 500;
+    }
+
+    & > span {
+      font-size: ${({ theme }) => theme.fontSize.sm};
+      color: ${({ theme }) => opacify(-0.35, theme.colors.neutral)};
+    }
+  }
+
+  & > button {
+    z-index: 1;
+  }
+`;
--- src/features/bills/routes/CreateBill/CreateBillPage.tsx
+++ src/features/bills/routes/CreateBill/CreateBillPage.tsx
@@ -1,18 +1,50 @@
+import { useState } from 'react';
+
 import { Button } from '@/components/elements';
 import { PageTitle } from '@/components/page-elements';
 
 import * as S from './CreateBillPage.styles';
 
+const steps = [
+  { title: 'Valores', description: 'Informe os valores' },
+  { title: 'Parcelas', description: 'Defina o parcelamento' },
+  { title: 'Devedores', description: 'Defina os devedores' },
+  { title: 'Resumo', description: 'Revise e confirme' }
+];
+
 export function CreateBillPage() {
+  const [currentStep, setCurrentStep] = useState(0);
+
   return (
     <S.Container>
       <PageTitle
         title="Lançamento de fatura"
         subtitle={`Preencha todos os campos e clique em "Avançar" para ir para a próxima etapa.`}
         showGoBackButton
-      >
-        <Button>Lançar fatura</Button>
-      </PageTitle>
+      />
+
+      <S.StepperWrapper>
+        <S.Stepper>
+          {steps.map((step, index) => (
+            <S.Step key={step.title} $isCurrentStep={index === currentStep}>
+              <main>
+                <strong>{step.title}</strong>
+                <span>{step.description}</span>
+              </main>
+
+              <Button
+                colorScheme={index === currentStep ? 'primary' : 'white'}
+                size="sm"
+                isRounded
+                onClick={() => setCurrentStep(index)}
+                style={{ pointerEvents: index === currentStep ? 'none' : 'initial' }}
+              >
+                {index + 1}
+              </Button>
+            </S.Step>
+          ))}
+        </S.Stepper>
+      </S.StepperWrapper>
     </S.Container>
   );
 }
